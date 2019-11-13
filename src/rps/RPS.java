255a6e5b4acbad19e3d56597df1671a9d06b5cf0
dymoo6791 <dymoo6791@7HGM6Y1.Student.UGDSB.ED>
2019-11-13 13:07:43 -0500

    removed score display Final product fully commented

--- src/rps/RPS.java
+++ src/rps/RPS.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Dylan Moore 
+ * November 13 2019
+ * Rock, paper, scissors program against computer, keeps score of user's wins, coms wins and ties
  */
 package rps;
 
@@ -20,49 +20,50 @@ public class RPS {
     static int userScore;
     static int comScore;
     static boolean run = true;
-    static int ties;
+    static int tieCounter;
 
     public static void main(String[] args) {
         userScore = 0;
         comScore = 0;
-        ties = 0;
+        tieCounter = 0;
         Random rand = new Random();
 
         while (run) {
             com = rand.nextInt(4 - 1) + 1; //random number generator for com (1-3)
-            System.out.println("-------------"); //Prompt user
+            System.out.println("-------------------------------------------"); //Prompt user
             System.out.println("Enter one of the following numbers to start");
             System.out.println("1 Rock");
             System.out.println("2 Paper");
             System.out.println("3 Scissors");
             user = in.nextInt(); //user input
 
-            userChoice(); //calls to user choice 
+            userChoice(); //calls to user choice method 
 
             comChoice(); //calls to method and returns com's choice based on random number
 
-            outputScores(); //updates score based on com and user choice
+            System.out.print("\tYour Score " + userScoreUpdate() + " Com Score " + comScoreUpdate() + " Ties " + tieCounter() + "\n");; //updates score based on com and user choice
 
             endProgram(); //end program when score reaches 5
         }
     }
-
-    /** outputs coms choice based on random num gen
+    /**
+     * outputs computers choice based on random number gen
      *
      */
     public static void comChoice() {
         final String[] OUTPUT = {"Rock", "Paper", "Scissors"};
         if (com == 1) {
-            System.out.println("Com picked " + OUTPUT[0]);
+            System.out.println("Com picked " + OUTPUT[0]); 
         } else if (com == 2) {
             System.out.println("Com picked " + OUTPUT[1]);
         } else if (com == 3) {
             System.out.println("Com picked " + OUTPUT[2]);
         }
     }
-/**
- * outputs user choice based on user input
- */
+
+    /**
+     * outputs user choice based on user input
+     */
     public static void userChoice() {
         final String[] OUTPUT = {"Rock", "Paper", "Scissors"};
         if (user == 1) {
@@ -73,10 +74,11 @@ public class RPS {
             System.out.println("You picked " + OUTPUT[2]);
         }
     }
+
     /**
-     * updates user score if user beat com
-     * 
-     * @return 
+     * updates user score if user beats com
+     *
+     * @return userScore
      */
     public static int userScoreUpdate() {
         if ((user == 1) && (com == 3)) {
@@ -92,14 +94,24 @@ public class RPS {
         return userScore;
     }
 
+    /**
+     * increases tie counter if user and com chose same option
+     *
+     * @return tieCounter
+     */
     public static int tieCounter() {
         if (com == user) {
             System.out.print("Tie");//if com choice is the same as user choice output "Tie"
-            ties++;
+            tieCounter++;
         }
-        return ties;
+        return tieCounter;
     }
 
+    /**
+     * updates user score if com beats user
+     *
+     * @return comScore
+     */
     public static int comScoreUpdate() {
         if ((com == 1) && (user == 3)) {
             comScore++;
@@ -114,19 +126,19 @@ public class RPS {
         return comScore;
     }
 
+    /**
+     * ends program if either score reaches 5
+     *
+     * @return run
+     */
     public static boolean endProgram() {
         if (userScore == 5) {
             run = false; //if user wins 5 games end program
             System.out.println("You Win!");
         } else if (comScore == 5) {
             run = false; //if com wins 5 games end program
-            System.out.println("Try Again");
+            System.out.print("Try Again");
         }
         return run;
-    }
-
-    public static void outputScores() {
-        System.out.print("\tYour Score " + userScoreUpdate() + " Com Score " + comScoreUpdate() + " Ties " + tieCounter() + "\n");
-
     }
 }
